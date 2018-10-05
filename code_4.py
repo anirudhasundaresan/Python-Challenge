0e615522d3a519fe370e72181dd1177993013177
anirudhasundaresan <anirudhasundaresan@outlook.com>
2018-10-05 01:37:18 -0500

    code_4 done

---
+++ code_4.py
@@ -0,0 +1,17 @@
+import requests
+import pprint
+import pdb
+
+# Follow the chain. Every new request starting from 12345 will give you a new URL, need to try them sequentially.
+url_new = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+next_ = '12345'
+
+for i in range(400):
+    url_new = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + next_
+    r = requests.get(url_new)
+    next_ = (r.content).split()[-1]
+    print(next_)
+
+'''
+python code_4.py | grep [A-Za-z] gives 'peak.html' as the link to the next challenge.
+'''
